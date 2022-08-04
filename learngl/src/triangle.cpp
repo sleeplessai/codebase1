32dbda46202590a295f4a8d4ca22f4b8aceea80e
sleeplessai <2018063+sleeplessai@users.noreply.github.com>
2022-08-04 21:17:20 +0800

    vbo,vao

--- learngl/src/triangle.cpp
+++ learngl/src/triangle.cpp
@@ -5,17 +5,11 @@
 
 #include <shader_s.h>
 
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main() {
+    /* Init */
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -33,42 +27,40 @@ int main() {
         std::cout << "Failed to initialize GLAD\n";
         return -1;
     }
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    /* rendering */
+    /* Render */
+    // triangle data on cpu
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
     };
+    // vbo
+    unsigned int vbo;
+    glGenBuffers(1, &vbo); //  &vbo can be array to gen multiple buffers
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);   // use learnopengl shader_s class
 
     // vao
     unsigned int vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao); // bind once
-
-    // vbo
-    unsigned int vbo;
-    glGenBuffers(1, &vbo); //  &vbo can be array to gen multiple buffers
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
     // location, vertex_abbr_value_num(vec3), dtype, norm_to_0-1, offset
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
-    // use learnopengl shader_s class
-    Shader shader("/home/suse/learn-gl/src/glsl/triangle.vs", "/home/suse/learn-gl/src/glsl/triangle.fs");
+    // shader
+    Shader shader("build/glsl/triangle.vs", "build/glsl/triangle.fs");
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
-        glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(vao);
         shader.use();
-        // glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
@@ -78,3 +70,11 @@ int main() {
     return 0;
 }
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
